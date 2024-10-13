0cf1e7e27cba9750df9b3eb1344b77061120bb45
raffasya <49897771+raffasyaa@users.noreply.github.com>
2024-10-13 12:36:38 +0000

    Update Tytyd_OP.sh

--- Tytyd_OP.sh
+++ Tytyd_OP.sh
@@ -4,6 +4,15 @@
 #
 # System Required:  CentOS 6+, Debian8+, Ubuntu16+
 
+# // Code for service
+export RED='\033[0;31m';
+export GREEN='\033[0;32m';
+export YELLOW='\033[0;33m';
+export BLUE='\033[0;34m';
+export PURPLE='\033[0;35m';
+export CYAN='\033[0;36m';
+export LIGHT='\033[0;37m';
+export NC='\033[0m';
 
 display_menu() {
     if [ $EUID -ne 0 ]; then
